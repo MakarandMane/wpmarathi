1b9e78680037c7c709dcb4af87028f79e9df589b
Yogesh Mane <yogesh@wpgenius.in>
2019-09-17 19:49:13 +0530

    Initial Commit

---
+++ wpgenius-translator.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: WPMarathi - Type in Matathi in WordPress
+Description: Type in Marathi inside WordPress. Hello -> हॅलो. WPMarathi helps you save time by letting you type inside Classic Editor and Gutenberg in Marathi.
+Author: Team WPGenius
+Author URI: https://tyche.work/plugins.gallery/
+Version: 1.4.0
+Requires at least: 5.0
+*/
+
+include(plugin_dir_path(__FILE__).'constant.php');
+include(plugin_dir_path(__FILE__).'class/wpgenius-transliterator.php');
+include(plugin_dir_path(__FILE__).'class/wpmarathi-deactivation-feedback.php');
+
+add_action( 'admin_enqueue_scripts', function($hook){
+    new WPGenius_Transliterator($hook);
+    new WPMarathi_Deactivation_Feedback($hook);
+});
+
+/*
+    Enqueue Required FrontEnd Scripts.
+*/
+add_action( 'wp_enqueue_scripts', function($hook){
+    wp_enqueue_style('wpmarthi-frontend',
+        plugin_dir_url(__FILE__).'/assets/css/wpmarathi-frontend.css',
+        null,
+        WPMARATHI_VERSION
+    );
+});
